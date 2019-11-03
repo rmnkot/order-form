b95cfe8024814749ad66d66ff0e611622b994243
rmnkot <r.kot@raise.band>
2019-11-04 00:25:34 +0200

    Add basic test configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -13,5 +12,3 @@ ReactDOM.render(
     </Provider>, 
     document.getElementById('root')
 );
-
-serviceWorker.unregister();
